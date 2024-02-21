a5578d6b401b4aade7dd9b85fd4512d5e2f84595
Rodrigo-Coloma <rjcolgut@gmail.com>
2024-02-21 08:25:39 +0100

    reformating

--- score_calculator.py
+++ score_calculator.py
@@ -1,5 +1,3 @@
-#We define a game class
-
 class Game():
     score = 0   
     round = 0
@@ -8,11 +6,11 @@ class Game():
     last_throw = 0
 
     def play(self,pins):
-        self.round += (self.throw +1) % 2
-        self.score += pins * (1 + self.strike[0] - (self.round > 10))
-        if self.throw == 0 and pins == 10 and (self.round < 11):
+        self.round += (self.throw + 1) % 2
+        self.score += pins * ((self.round <= 10) + self.strike[0])
+        if self.throw == 0 and pins == 10 and (self.round <= 10):
             self.strike = (self.strike[1]+1,1)
-        elif self.throw == 1 and ((pins + self.last_throw) == 10) and (self.round < 11):
+        elif self.throw == 1 and ((pins + self.last_throw) == 10) and (self.round <= 10):
             self.strike = (self.strike[1]+1,0)
             self.throw = 0
         else:
@@ -25,7 +23,7 @@ class Game():
             if 'x' in sequence_part:
                 punctuation =int(sequence_part.split('x')[-1]) 
                 times = int(sequence_part.split('x')[0])
-                [Game.play(self, punctuation) for n in range(times)]
+                [self.play(punctuation) for n in range(times)]
             else:
-                Game.play(self,int(sequence_part))
+                self.play(int(sequence_part))
         return self.score
